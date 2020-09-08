2371b1bc79367645f2f5bb2d2059499ee4988b9c
Liubomyr <louidropson@gmail.com>
2020-09-08 16:02:05 +0300

    Updates

---
+++ precache-manifest.3a3698cb17e08a44b3b21e97b3f8efcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65334b8f68a9439c18e7619c64a03317",
+    "url": "/cursor-react-3/index.html"
+  },
+  {
+    "revision": "2e4bec8a7219d064d054",
+    "url": "/cursor-react-3/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "86719fdeffd649c92786",
+    "url": "/cursor-react-3/static/js/2.2e0fd4fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-react-3/static/js/2.2e0fd4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4bec8a7219d064d054",
+    "url": "/cursor-react-3/static/js/main.479e887a.chunk.js"
+  },
+  {
+    "revision": "558211fd691fecf03460",
+    "url": "/cursor-react-3/static/js/runtime-main.39c25985.js"
+  }
+]);
